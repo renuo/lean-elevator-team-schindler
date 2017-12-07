b664465d6d9f152cd128e92ec74b086ac55e9350
Simon Huber <simon.huber@renuo.ch>
2017-12-07 15:40:08 +0100

    most people at floor strategy

--- main.rb
+++ main.rb
@@ -14,9 +14,17 @@ post '/decide' do
 
   # Do the calculations
   puts "Got: \n#{params_json}"
-  target_level = params_json['elevator']['target_floors'].first || first_floor_with_people(params_json['floors'])
-
+  target_level = floor_decision(params_json)
   target_level.to_json
+end
+
+def floor_decision(params_json)
+  target_floors = params_json['elevator']['target_floors']
+  target_floors.empty? ? first_floor_with_people(params_json['floors']) : floor_most_people_want_to_leave(target_floors)
+end
+
+def floor_most_people_want_to_leave(target_floors)
+  target_floors.inject(Hash.new(0)) {|h, i| h[i] += 1; h}.sort {|a1, a2| a2[1]<=>a1[1]}.first.first
 end
 
 def first_floor_with_people(floors)
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,5 +1,94 @@
 require File.expand_path '../spec_helper.rb', __FILE__
 
+
+def response
+  {
+    "elevator": {
+      "capacity": 6,
+      "current_floor": 8,
+      "target_floors": [],
+      "statistics": 15273
+    },
+    "floors": [{
+                 "panel": {
+                   "up": false,
+                   "down": false
+                 }
+               }, {
+                 "panel": {
+                   "up": false,
+                   "down": false
+                 }
+               }, {
+                 "panel": {
+                   "up": true,
+                   "down": false
+                 }
+               }, {
+                 "panel": {
+                   "up": true,
+                   "down": true
+                 }
+               }, {
+                 "panel": {
+                   "up": false,
+                   "down": false
+                 }
+               }, {
+                 "panel": {
+                   "up": true,
+                   "down": false
+                 }
+               }, {
+                 "panel": {
+                   "up": false,
+                   "down": false
+                 }
+               }, {
+                 "panel": {
+                   "up": true,
+                   "down": false
+                 }
+               }, {
+                 "panel": {
+                   "up": false,
+                   "down": false
+                 }
+               }, {
+                 "panel": {
+                   "up": false,
+                   "down": true
+                 }
+               }, {
+                 "panel": {
+                   "up": false,
+                   "down": true
+                 }
+               }, {
+                 "panel": {
+                   "up": false,
+                   "down": true
+                 }
+               }, {
+                 "panel": {
+                   "up": false,
+                   "down": true
+                 }
+               }, {
+                 "panel": {
+                   "up": false,
+                   "down": false
+                 }
+               }, {
+                 "panel": {
+                   "up": false,
+                   "down": false
+                 }
+               }]
+  }
+end
+
+
 describe 'The simple main app' do
   it 'allows access to the home page' do
     get '/'
@@ -7,7 +96,9 @@ describe 'The simple main app' do
   end
 
   it 'allows to access the decider' do
-    post '/decide', {state: 'blub'}.to_json
+    post '/decide', response.to_json
     expect(last_response).to be_ok
+    expect(last_response.body).to eq('')
+
   end
 end
